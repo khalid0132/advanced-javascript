27914d6ded65ee101e7030a6bba63dcdb6a46330
Khalid Saifullah <khalid0132@gmail.com>
2021-02-01 19:30:51 +0100

    advanced javascript

---
+++ true-false.js
@@ -0,0 +1,31 @@
+// const age = -2; // but age er value 0 holei false
+const age = 0; // but age er value 0 holei false
+if (age) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// stringe er jonno
+
+// const name = "khan";
+// const name1 = ""; // empty string hole false but empty string but white space hale abar kintu true
+const name1 = " "; // empty string hole false but empty string but white space hale abar kintu true
+if (name1) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// conditions are false
+// // 1. null value
+// 2. empty String
+// 3. undefined value
+// 4. NaN value
+// 5. value = false
+
+// conditions are true
+// 1. '0' it is a String
+// 2. [] it is also true
+// 3. object {}
+// 4. 'flase'
