a52dedc2a465e9ff30aec330c3bd13a5e637da42
Khalid Saifullah <khalid0132@gmail.com>
2021-02-01 20:35:06 +0100

    == vs ===

---
+++ double-vs-triple.js
@@ -0,0 +1,18 @@
+// const first = 4;
+// const second = "4";
+// if (first === second) {
+//   // both will check data type and value
+//   console.log("condition is true");
+// } else {
+//   console.log("condition is false");
+// }
+
+const first = 0;
+const second = false;
+// if(first == false)
+if (first === second) {
+  // both will check data type and value
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
